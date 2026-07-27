77e14f3ed7260babc0b91db6028134e744d6d5f8
hutuyee <hutuyee@outlook.com>
2026-07-27 13:43:34 +0800

    init

---
+++ ShitBotVelocity/src/main/java/ds/shitBotVelocity/ShitBotVelocity.java
@@ -0,0 +1,17 @@
+package ds.shitBotVelocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import org.slf4j.Logger;
+
+public class ShitBotVelocity {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        // Plugin initialization logic goes here
+    }
+}
